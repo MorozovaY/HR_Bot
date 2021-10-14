9f2a1d6634eecae1df0fa5cfec3f524283516dac
Yulia <1206bogomazovaja@gmail.com>
2021-10-14 19:26:11 +0300

    employee_keyboard added

--- handlers.py
+++ handlers.py
@@ -1,5 +1,6 @@
 from telegram import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from candidate_keyboard import candidate_keyboard
+from employee_keyboard import employee_keyboard
 
 def greet_user(update, context):
     print('Вызван /start')
@@ -17,6 +18,7 @@ def main_keyboard():
         [
             ['Регистрация'],
             ['Войти по ключу кандидата'],
+            ['Войти по ключу сотрудника'],
             ['О компании', 'Офисы', 'Соцсети и контакты']
         ])
 
@@ -26,6 +28,14 @@ def enter_candidate(update, context):
     photo='https://previews.123rf.com/images/maxkabakov/maxkabakov1210/maxkabakov121000043/15856739-%E6%83%85%E5%A0%B1%E6%8A%80%E8%A1%93-it-%E3%81%AE%E6%A6%82%E5%BF%B5-%E3%83%87%E3%82%B8%E3%82%BF%E3%83%AB-%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%80%813-d-%E3%81%AE%E3%83%AC%E3%83%B3%E3%83%80%E3%83%AA%E3%83%B3%E3%82%B0%E3%81%AB%E3%83%94%E3%82%AF%E3%82%BB%E3%83%AB%E5%8C%96%E3%81%95%E3%82%8C%E3%81%9F%E8%A8%80%E8%91%89%E3%82%92%E6%AD%93%E8%BF%8E.jpg',
     caption='Поздравляем с успешным прохождением всех этапов отбора! Ниже представлено меню бота для Вас.',
     reply_markup=candidate_keyboard()
+)
+
+
+def enter_employee(update, context):
+    context.bot.sendPhoto(chat_id=update.effective_chat.id,
+    photo='https://www.assmont.com/wp-content/uploads/2020/04/1Karriere2.jpg',
+    caption='Welcome on board! Ниже представлено меню бота для Вас.',
+    reply_markup=employee_keyboard()
 )
 
 
